85f559da718f369ecb49023a0451c9226b3cdb23
R46narok <stanimirkolev04@gmail.com>
2022-11-15 13:38:24 +0200

    :construction: Added http api for exercises

--- src/Services/Skeletal/ZeroGravity.Services.Skeletal.Api/Controllers/ExerciseController.cs
+++ src/Services/Skeletal/ZeroGravity.Services.Skeletal.Api/Controllers/ExerciseController.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ZeroGravity.Services.Skeletal.Commands.Exercises.CreateExercise;
+using ZeroGravity.Services.Skeletal.Commands.Exercises.DeleteExercise;
+using ZeroGravity.Services.Skeletal.Commands.Exercises.UpdateExercise;
 using ZeroGravity.Services.Skeletal.Queries.GetAllExercises;
 
 namespace ZeroGravity.Services.Skeletal.Api.Controllers;
@@ -16,6 +18,7 @@ public class ExerciseController : ControllerBase
     }
 
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<IActionResult> GetAllExercisesAsync()
     {
         var query = new GetAllExercisesQuery();
@@ -24,7 +27,26 @@ public class ExerciseController : ControllerBase
     }
 
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> CreateExerciseAsync([FromBody] CreateExerciseCommand command)
+    {
+        var response = await _mediator.Send(command);
+        return Application.StatusCode.ToObjectResult(response);
+    }
+    
+    [HttpPatch]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> UpdateExerciseAsync([FromBody] UpdateExerciseCommand command)
+    {
+        var response = await _mediator.Send(command);
+        return Application.StatusCode.ToObjectResult(response);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteExerciseAsync([FromBody] DeleteExerciseCommand command)
     {
         var response = await _mediator.Send(command);
         return Application.StatusCode.ToObjectResult(response);
