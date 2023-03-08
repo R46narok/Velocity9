3c6084bd879535c0b5b279e8c43d1d04b46f4129
R46narok <stanimirkolev04@gmail.com>
2023-03-08 11:04:21 +0200

    :construction: Added page for workout generation. Needs wiring.

---
+++ src/UI/Portal/ZeroGravity.UI.Portal.Blazor/Pages/Workout/Generate/Common/DisplayComponentControl.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ZeroGravity.UI.Portal.Blazor.Pages.Workout.Generate.Common;
+
+public class DisplayComponentControl : ComponentBase
+{
+    [Parameter]
+    public Action? ControlRequested { get; set; }
+}
