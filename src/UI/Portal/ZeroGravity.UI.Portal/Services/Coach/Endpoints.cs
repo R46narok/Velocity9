eaa322ddf240a72ea12aab491af6256943984df1
R46narok <stanimirkolev04@gmail.com>
2023-03-25 19:01:11 +0200

    :sparkles: Added Coach.Api consumption in UI.Portal.Blazor, closes #46

---
+++ src/UI/Portal/ZeroGravity.UI.Portal/Services/Coach/Endpoints.cs
@@ -0,0 +1,7 @@
+﻿namespace ZeroGravity.UI.Portal.Services.Coach;
+
+public static class Endpoints
+{
+    public const string AnomalyBase = "/api/anomaly";
+    public const string Anomaly = AnomalyBase;
+}
